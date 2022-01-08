fbece565586f88ea82d337b218e7695322519455
Kelli K <kjkleindorfer@dmacc.edu>
2022-01-08 14:11:29 -0600

    initial commit

---
+++ src/AnimalNoises.java
@@ -0,0 +1,17 @@
+import model.Cat;
+
+public class AnimalNoises {
+	
+	// add your animal class to the model package
+	// only push this file and the animal class you created
+
+	public static void main(String[] args) {
+		Cat garfield = new Cat();
+		System.out.println(garfield.speak());
+		
+		
+		
+
+	}
+
+}
